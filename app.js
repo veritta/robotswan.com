e8ebb7d072ffc945b66787ab14c34dcadb954f63
Sunny Lai <arterialist@gmail.com>
2015-12-06 10:28:56 +0100

    made app.js and package.json work with openshift

--- app.js
+++ app.js
@@ -47,7 +47,11 @@ pages.forEach(function(page) {
 	});
 });
 
-var server = app.listen(8000, function () {
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8000;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
+var server = app.listen(server_port, server_ip_address, function () {
 	var port = server.address().port;
 
 	console.log("App listening on port %s", port);
